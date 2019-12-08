6ff7064efe6e2cf1da6b605a55d8f26a4554fcad
louhwz <872188659@qq.com>
2019-12-08 11:06:49 +0800

    1、dockerfile 2、add build_jar.sh

---
+++ build_jar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      Louhwz                               $'
+echo '$                                                           $'
+echo '$  email:    19212010032@fudan.edu.cn                       $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+
+cd micro_sbdata
+mvn clean package -DskipTests
